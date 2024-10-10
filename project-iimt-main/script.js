d7215fa26271b59bd80ead9c70c811510a94c798
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-10-10 20:43:13 +0530

    updated

--- project-iimt-main/script.js
+++ project-iimt-main/script.js
@@ -72,7 +72,7 @@ const loaderPlay = () => {
   // Loop through translations and add them to the timeline in sequence
   translations.forEach((translation) => {
     tl.to(loaderText, {
-      duration: 0.2,
+      duration: 0.12,
       opacity: 0,
       onComplete: () => {
         loaderText.textContent = translation.text;
@@ -95,14 +95,16 @@ const loaderPlay = () => {
     y: "-100%",
     duration: 1,
     ease: "power3.out",
-    delay: 0.65,
+    delay: 0.64,
   });
   tl.from(".iimt", {
-    x: "-15%",
+    x: "-10%",
+    duration: .2,
     opacity: 0,
   });
   tl.from(".showcase", {
-    y: "20%",
+    y: "15%",
+    duration: .2,
     opacity: 0,
   });
 };
@@ -169,3 +171,40 @@ loaderPlay();
 customCursor();
 textSplitting();
 scroll();
+
+Shery.imageEffect(".image-div", {
+  style: 6,
+  // debug: true,
+  config: {
+    a: { value: 2, range: [0, 30] },
+    b: { value: 0.75, range: [-1, 1] },
+    zindex: { value: 9996999, range: [-9999999, 9999999] },
+    aspect: { value: 0.7917349903853899 },
+    ignoreShapeAspect: { value: true },
+    shapePosition: { value: { x: 0, y: 0 } },
+    shapeScale: { value: { x: 0.5, y: 0.5 } },
+    shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+    shapeRadius: { value: 0, range: [0, 2] },
+    currentScroll: { value: 0 },
+    scrollLerp: { value: 0.07 },
+    gooey: { value: true },
+    infiniteGooey: { value: false },
+    growSize: { value: 4, range: [1, 15] },
+    durationOut: { value: 1, range: [0.1, 5] },
+    durationIn: { value: 1.5, range: [0.1, 5] },
+    displaceAmount: { value: 0.5 },
+    masker: { value: true },
+    maskVal: { value: 1.2, range: [1, 5] },
+    scrollType: { value: 0 },
+    geoVertex: { range: [1, 64], value: 1 },
+    noEffectGooey: { value: true },
+    onMouse: { value: 0 },
+    noise_speed: { value: 0.69, range: [0, 10] },
+    metaball: { value: 0.56, range: [0, 2] },
+    discard_threshold: { value: 0.5, range: [0, 1] },
+    antialias_threshold: { value: 0, range: [0, 0.1] },
+    noise_height: { value: 0.3, range: [0, 2] },
+    noise_scale: { value: 10, range: [0, 100] },
+  },
+  gooey: true,
+});
