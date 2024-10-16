8b2aab21c4da1a500de285ffac48bafab52dcb54
V <160890362+vivek28Jaiswal@users.noreply.github.com>
2024-10-16 11:52:41 +0530

    responsive

--- project-iimt-main/script.js
+++ project-iimt-main/script.js
@@ -160,51 +160,55 @@ const scroll = () => {
     },
   });
 };
-Shery.imageEffect(".right-img img", {
-  style: 6,
-  // debug: true,
-  config: {"noiseDetail":{"value":6.87,"range":[0,100]},"distortionAmount":{"value":3.05,"range":[0,10]},"scale":{"value":49.62,"range":[0,100]},"speed":{"value":0.5,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.8333389902938148},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
-});
-
+const wiggleEffect =() => {
+  Shery.imageEffect(".right-img img", {
+    style: 6,
+    // debug: true,
+    config: {"noiseDetail":{"value":6.87,"range":[0,100]},"distortionAmount":{"value":3.05,"range":[0,10]},"scale":{"value":49.62,"range":[0,100]},"speed":{"value":0.5,"range":[0,1]},"zindex":{"value":-9996999,"range":[-9999999,9999999]},"aspect":{"value":0.8333389902938148},"ignoreShapeAspect":{"value":true},"shapePosition":{"value":{"x":0,"y":0}},"shapeScale":{"value":{"x":0.5,"y":0.5}},"shapeEdgeSoftness":{"value":0,"range":[0,0.5]},"shapeRadius":{"value":0,"range":[0,2]},"currentScroll":{"value":0},"scrollLerp":{"value":0.07},"gooey":{"value":false},"infiniteGooey":{"value":false},"growSize":{"value":4,"range":[1,15]},"durationOut":{"value":1,"range":[0.1,5]},"durationIn":{"value":1.5,"range":[0.1,5]},"displaceAmount":{"value":0.5},"masker":{"value":false},"maskVal":{"value":1,"range":[1,5]},"scrollType":{"value":0},"geoVertex":{"range":[1,64],"value":1},"noEffectGooey":{"value":true},"onMouse":{"value":0},"noise_speed":{"value":0.2,"range":[0,10]},"metaball":{"value":0.2,"range":[0,2]},"discard_threshold":{"value":0.5,"range":[0,1]},"antialias_threshold":{"value":0.002,"range":[0,0.1]},"noise_height":{"value":0.5,"range":[0,2]},"noise_scale":{"value":10,"range":[0,100]}}
+  });
+}
+const gooeyEffect =() => {
+  Shery.imageEffect(".image-div", {
+    style: 6,
+    // debug: true,
+    config: {
+      a: { value: 2, range: [0, 30] },
+      b: { value: 0.75, range: [-1, 1] },
+      zindex: { value: 9996999, range: [-9999999, 9999999] },
+      aspect: { value: 0.7917349903853899 },
+      ignoreShapeAspect: { value: true },
+      shapePosition: { value: { x: 0, y: 0 } },
+      shapeScale: { value: { x: 0.5, y: 0.5 } },
+      shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
+      shapeRadius: { value: 0, range: [0, 2] },
+      currentScroll: { value: 0 },
+      scrollLerp: { value: 0.07 },
+      gooey: { value: true },
+      infiniteGooey: { value: false },
+      growSize: { value: 4, range: [1, 15] },
+      durationOut: { value: 1, range: [0.1, 5] },
+      durationIn: { value: 1.5, range: [0.1, 5] },
+      displaceAmount: { value: 0.5 },
+      masker: { value: true },
+      maskVal: { value: 1.2, range: [1, 5] },
+      scrollType: { value: 0 },
+      geoVertex: { range: [1, 64], value: 1 },
+      noEffectGooey: { value: true },
+      onMouse: { value: 0 },
+      noise_speed: { value: 0.69, range: [0, 10] },
+      metaball: { value: 0.56, range: [0, 2] },
+      discard_threshold: { value: 0.5, range: [0, 1] },
+      antialias_threshold: { value: 0, range: [0, 0.1] },
+      noise_height: { value: 0.3, range: [0, 2] },
+      noise_scale: { value: 10, range: [0, 100] },
+    },
+    gooey: true,
+  });
+}
 locomotive();
 loaderPlay();
 customCursor();
 textSplitting();
 scroll();
-
-Shery.imageEffect(".image-div", {
-  style: 6,
-  // debug: true,
-  config: {
-    a: { value: 2, range: [0, 30] },
-    b: { value: 0.75, range: [-1, 1] },
-    zindex: { value: 9996999, range: [-9999999, 9999999] },
-    aspect: { value: 0.7917349903853899 },
-    ignoreShapeAspect: { value: true },
-    shapePosition: { value: { x: 0, y: 0 } },
-    shapeScale: { value: { x: 0.5, y: 0.5 } },
-    shapeEdgeSoftness: { value: 0, range: [0, 0.5] },
-    shapeRadius: { value: 0, range: [0, 2] },
-    currentScroll: { value: 0 },
-    scrollLerp: { value: 0.07 },
-    gooey: { value: true },
-    infiniteGooey: { value: false },
-    growSize: { value: 4, range: [1, 15] },
-    durationOut: { value: 1, range: [0.1, 5] },
-    durationIn: { value: 1.5, range: [0.1, 5] },
-    displaceAmount: { value: 0.5 },
-    masker: { value: true },
-    maskVal: { value: 1.2, range: [1, 5] },
-    scrollType: { value: 0 },
-    geoVertex: { range: [1, 64], value: 1 },
-    noEffectGooey: { value: true },
-    onMouse: { value: 0 },
-    noise_speed: { value: 0.69, range: [0, 10] },
-    metaball: { value: 0.56, range: [0, 2] },
-    discard_threshold: { value: 0.5, range: [0, 1] },
-    antialias_threshold: { value: 0, range: [0, 0.1] },
-    noise_height: { value: 0.3, range: [0, 2] },
-    noise_scale: { value: 10, range: [0, 100] },
-  },
-  gooey: true,
-});
+wiggleEffect();
+gooeyEffect();
